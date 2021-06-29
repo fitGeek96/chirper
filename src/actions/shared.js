7fb278578d5d47bf5821e6373d04c85a16cd20bc
fitgeek96 <fitgeek96@gmail.com>
2021-06-29 10:02:41 +0300

    Reducers and Middleware

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -9,7 +9,7 @@ const AUTHED_ID = 'tylermcginnis';
 export function handleInitialData() {
     return (dispatch) => {
         return getInitialData()
-               .then(({ users, tweets }) =>{
+               .then(({ users, tweets }) => {
                     dispatch(receiveUsers(users));
                     dispatch(receiveTweets(tweets));
                     dispatch(setAuthedUser(AUTHED_ID));
---
+++ src/middleware/index.js
@@ -0,0 +1,15 @@
+import redux from 'redux'
+
+
+const logger = (store) => (next) => (action) => {
+    console.group(action.type)
+        console.log("The action : ", action);
+        const returnValue = next(action);
+        console.log("The new State : ", store.getState());
+    console.groupEnd();
+
+    return returnValue;
+}
+
+
+export default logger;
