45448a2afa547674b40ea444a05e06a0a4fcea34
fitgeek96 <fitgeek96@gmail.com>
2021-06-29 12:42:44 +0300

    Dashboard Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
+import { func } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
-
+import Dashboard from './Dashboard';
 class App extends Component {
 
   componentDidMount() {
@@ -11,10 +12,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+        {this.props.loading === true ? null :  <Dashboard />}
       </div>
     )
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+
+export default connect(mapStateToProps)(App);
