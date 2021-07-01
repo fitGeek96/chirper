07d0047dcb8f278113970de5e5622df185e76fb7
fitgeek96 <fitgeek96@gmail.com>
2021-07-01 07:39:51 +0300

    Add New Tweet Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,39 @@
-import { func } from 'prop-types';
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { handleInitialData } from '../actions/shared';
+import {func} from 'prop-types';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {handleInitialData} from '../actions/shared';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import NewTweet from './NewTweet';
+import TweetPage from './TweetPage';
 class App extends Component {
 
-  componentDidMount() {
-    this.props.dispatch(handleInitialData());
-  }
+    componentDidMount() {
+        this
+            .props
+            .dispatch(handleInitialData());
+    }
 
-  render() {
-    return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true ? null :  <Dashboard />}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <LoadingBar/> {this.props.loading === true
+                    ? null
+                    : <TweetPage
+                        match={{
+                        params: {
+                            id: "f4xzgapq7mu783k9t02ghx"
+                        }
+                    }}/>}
+            </div>
+        )
+    }
 }
 
-function mapStateToProps({ authedUser }) {
-  return {
-    loading: authedUser === null
-  }
+function mapStateToProps({authedUser}) {
+    return {
+        loading: authedUser === null
+    }
 }
-
 
 export default connect(mapStateToProps)(App);
